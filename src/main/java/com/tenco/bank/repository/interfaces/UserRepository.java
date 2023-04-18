c9e79e1cd964cc68bee2dc7fd8b6bb2d1c407495
HyooRin <khl4459@naver.com>
2023-04-18 12:35:43 +0900

    회원가입 트랜잭션처리

--- src/main/java/com/tenco/bank/repository/interfaces/UserRepository.java
+++ src/main/java/com/tenco/bank/repository/interfaces/UserRepository.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.tenco.bank.dto.SignUpFormDto;
 import com.tenco.bank.repository.model.User;
 @Mapper // MyBatis 의존설정함 (build.gradle 파일)
 public interface UserRepository {
-	
-	public int insert(User user);
+	// 코드 수정하기 ㄴ
+	public int insert(SignUpFormDto signUpFormDto);
 	public int updateById(User user);
 	public int deleteById(Integer id);
 	public User findById(Integer id);
